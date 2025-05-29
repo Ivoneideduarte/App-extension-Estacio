25a946784512bad999d18a85e0f518704f7a1ac7
Ivoneideduarte <contato.ivoneideduarte@gmail.com>
2025-05-28 21:40:43 -0300

    Atualizei a pasta com meu projeto

---
+++ components/header/index.tsx
@@ -0,0 +1,41 @@
+import { Feather } from '@expo/vector-icons';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, StatusBar } from 'react-native';
+import { colors } from '@/constants/colors';
+
+interface HeaderProps{
+    user: String;
+}
+
+export default function Header({user} : HeaderProps) {
+    return (
+        <SafeAreaView style={styles.header}>
+
+            <Text style={styles.user}>Olá, {user}</Text>
+
+            <TouchableOpacity>
+                <Feather name="user" size={30} color='#0D1321' />
+            </TouchableOpacity>
+
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 1,
+        maxHeight: 180,
+        backgroundColor: colors.blue,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 24,
+        paddingTop: 30,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+    },
+    user: {
+        fontSize: 20,
+        fontWeight: 'semibold',
+        color: '#0D1321'
+    }
+})
---
+++ constants/colors.tsx
@@ -0,0 +1,7 @@
+
+export const colors = {
+    background: '#f1f1f1',
+    white: '#fff',
+    black: '#000',
+    blue: '#97D2FB'
+}
